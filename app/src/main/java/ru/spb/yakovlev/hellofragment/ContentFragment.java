08131062d54ee49780ca8fecee5d7e7e4664a4ce
AlekseyYakovlev <aleksey@yakovlev.spb.ru>
2018-02-12 23:02:55 +0300

    Initial commit

---
+++ app/src/main/java/ru/spb/yakovlev/hellofragment/ContentFragment.java
@@ -0,0 +1,49 @@
+package ru.spb.yakovlev.hellofragment;
+
+import android.support.v4.app.FragmentTransaction;
+import android.view.View;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Date;
+
+public class ContentFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_content, container, false);
+
+        Button updateButton = view.findViewById(R.id.updateButton);
+        Button switchButton = view.findViewById(R.id.switchButton);
+
+        final TextView updateBox =  view.findViewById(R.id.textBox);
+
+        updateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String curDate = new Date().toString();
+                updateBox.setText(curDate);
+            }
+        });
+
+        switchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ContentFragment1 detailFragment = new ContentFragment1();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.addToBackStack(null);
+                transaction.replace(R.id.firstFragment, detailFragment);
+                transaction.commit();
+            }
+        });
+
+
+
+        return view;
+    }
+}
